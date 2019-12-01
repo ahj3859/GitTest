34437491b087d97930d2ae654bdfb266a37f132b
ahj3859 <ahj3859@gmail.com>
2019-12-01 20:38:00 +0900

    #1 commit!

---
+++ src/test/java/com/jojoldu/springwebservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.springwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
