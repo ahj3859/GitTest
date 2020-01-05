86e14a083597ad78dfc2ed276cbb06a34d9a3274
ahn <ahj3859@gmail.com>
2020-01-06 01:44:53 +0900

    change external yml location

--- src/main/java/com/jojoldu/webservice/Application.java
+++ src/main/java/com/jojoldu/webservice/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
-			+ "~/app/config/springboot-webservice/real-application.yml";
+			+ "/app/config/springboot-webservice/real-application.yml";
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class)
